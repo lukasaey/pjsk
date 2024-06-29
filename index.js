90851469bbafd760735c9404191d3b4a3067cc30
lukasaey <lukagamercoll@gmail.com>
2024-06-29 22:47:30 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function openTab(event, tabName) {
+  // Get all elements with class="tabcontent" and hide them
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  let tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+function onSubmit(event) {
+  fetch("events.json").then(async (events) => {
+    const json = await events.json()
+    console.log(json)
+  })
+}
